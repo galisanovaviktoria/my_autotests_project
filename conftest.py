5de4e593ba04a264750f00ceb2988ecc066e2f08
galisanovaviktoria <galisanovaviktoria@gmail.com>
2025-10-30 15:57:10 +0300

    Корректировка кода 3

--- conftest.py
+++ conftest.py
@@ -1,7 +1,6 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 
 
 @pytest.fixture
@@ -10,13 +9,9 @@ def browser():
     chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--window-size=1920,1080")
-    chrome_options.add_argument("--disable-extensions")
 
-    # Используем системный chromedriver
-    service = Service("/usr/bin/chromedriver")
-    driver = webdriver.Chrome(service=service, options=chrome_options)
+    driver = webdriver.Chrome(options=chrome_options)
     driver.implicitly_wait(10)
 
     yield driver
