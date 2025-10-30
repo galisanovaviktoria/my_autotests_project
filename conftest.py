4b49844fa6d4bbd3f3fc1442b7e21f6cf461b280
galisanovaviktoria <galisanovaviktoria@gmail.com>
2025-10-30 15:41:33 +0300

    Внесение корректировок к код

--- conftest.py
+++ conftest.py
@@ -1,23 +1,19 @@
-import os
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture
 def browser():
-    options = Options()
-    # headless режим для CI
-    if os.getenv("CI", "false").lower() == "true":
-        options.add_argument("--headless=new")
-        options.add_argument("--no-sandbox")
-        options.add_argument("--disable-dev-shm-usage")
-        options.add_argument("--window-size=1920,1080")
-    else:
-        options.add_argument("--window-size=1920,1080")
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--window-size=1920,1080")
 
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.implicitly_wait(10)
+
     yield driver
     driver.quit()
